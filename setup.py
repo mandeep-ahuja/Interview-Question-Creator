baa8a97a1df5a2765a7aa0a0726362d12b3ee1cf
mandeep-ahuja <mandeep.ahuja08@gmail.com>
2024-06-02 20:02:07 -0400

    reuirements.txt updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenAI Project",
+    version= "0.0.0",
+    author="Mandeep Ahuja",
+    email="mandeep.ahuja08@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
